6298b4ea0842dfd1c304cd4b9658bac630428e11
TIBET7 <lorneras@gmail.com>
2020-05-06 13:31:30 +0300

    change is_even.js eslint comments

--- src/is_even.js
+++ src/is_even.js
@@ -12,18 +12,18 @@ console.log(`${'Hello, '}${userName}${'!'}`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 export default () => {
-  const randomNum = Math.floor(Math.random() * 1000);
-  const userAnswer = readlineSync.question(`${'Question: '}${randomNum}${'   '}`);
+  let randomNum = Math.floor(Math.random() * 1000);
+  let userAnswer = readlineSync.question(`${'Question: '}${randomNum}${'   '}`);
   console.log(`${'Your answer: '}${userAnswer}`);
   if (userAnswer === isEven(randomNum)) {
     console.log('Correct!');
-    const randomNum = Math.floor(Math.random() * 1000);
-    const userAnswer = readlineSync.question(`${'Question: '}${randomNum}${'   '}`);
+    randomNum = Math.floor(Math.random() * 1000);
+    userAnswer = readlineSync.question(`${'Question: '}${randomNum}${'   '}`);
     console.log(`${'Your answer: '}${userAnswer}`);
     if (userAnswer === isEven(randomNum)) {
       console.log('Correct!');
-      const randomNum = Math.floor(Math.random() * 1000);
-      const userAnswer = readlineSync.question(`${'Question: '}${randomNum}${'   '}`);
+      randomNum = Math.floor(Math.random() * 1000);
+      userAnswer = readlineSync.question(`${'Question: '}${randomNum}${'   '}`);
       console.log(`${'Your answer: '}${userAnswer}`);
       if (userAnswer === isEven(randomNum)) {
         console.log('Correct!');
