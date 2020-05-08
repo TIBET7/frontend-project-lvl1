716057653fc35eb97e1151f7cfb30e53c8e9176a
TIBET7 <lorneras@gmail.com>
2020-05-08 15:35:45 +0300

    add working code in calc.js

--- src/calc.js
+++ src/calc.js
@@ -1,6 +1,27 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`${'Hello, '}${userName}${'!'}`);
-console.log('What is the result of the expression?');
+export default () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`${'Hello, '}${userName}${'!'}`);
+  console.log('What is the result of the expression?');
+  for (let i = 0; i < 3;) {
+    const randomNum1 = Math.floor(Math.random() * 10);
+    const randomNum2 = Math.floor(Math.random() * 10);
+    const arr = ['+', '-', '*'];
+    const randomOperator = arr[Math.floor(Math.random() * arr.length)];
+    const userAnswer = readlineSync.question(`Question: ${randomNum1}${randomOperator}${randomNum2}  `);
+    const correctAnswer = eval(`${randomNum1}${randomOperator}${randomNum2}`);
+    if (Number(userAnswer) === correctAnswer) {
+      console.log(`Your answer: ${userAnswer}`);
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log(`"${userAnswer}" is wrong answer. Correct answer was "${correctAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
+      return false;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+  return true;
+};
